244bf5887d2da2517f22a4601b6ab3d1fae03b5c
gsai3939 <Saig7739@gmail.com>
2025-08-07 00:18:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+export default {
+  base: 'https://github.com/gsai3939/Portfolio', // 
+}
 export default defineConfig({
   plugins: [react()],
 });
